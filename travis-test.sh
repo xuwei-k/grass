215d2f434ef0ecbb68491ab544521fd4b20c8d3a
xuwei-k <6b656e6a69@gmail.com>
2013-09-06 18:23:25 +0900

    add sbt 0.13.0

--- travis-test.sh
+++ travis-test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wget https://raw.github.com/paulp/sbt-extras/master/sbt &&
 chmod u+x ./sbt &&
-./sbt -sbt-version 0.12.3 scripted
+./sbt -sbt-version 0.12.3 scripted &&
+./sbt -sbt-version 0.13.0 scripted
